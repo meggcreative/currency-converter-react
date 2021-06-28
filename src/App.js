49cc68c64f85b6471c4395220ef6bad08d235427
Magdalena Zulewska <madziapernach@gmail.com>
2021-06-28 17:09:31 +0200

    component Clock added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Container from "./Container";
 import Form from "./Form";
 import currencies from "./Currencies/currencies";
+import Clock from "./Clock";
 
 
 function App() {
-
   const [result, setResult] = useState();
-
   const calculateResult = (amount, currency) => {
-    const rate = currencies.find(({short}) => short === currency).rate;
-
+    const rate = currencies.find(({ short }) => short === currency).rate;
     setResult({
       givenAmount: +amount,
-      targetAmount: amount/rate,
+      targetAmount: amount / rate,
       currency,
     });
   }
   return (
     <>
-    <Header title="KALKULATOR WALUT" />
-    
-    <Container> 
-      <Form calculateResult={calculateResult} result={result}/>
-    </Container>
+      <Header title="KALKULATOR WALUT" />
+
+      <Container>
+        <Clock />
+        <Form calculateResult={calculateResult} result={result} />
+      </Container>
     </>
   );
 }
---
+++ src/Clock/index.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import "./style.css";
+
+const Clock = () => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [])
+
+    return (
+        <div className="clock">
+            Dzisiaj jest:
+            {" "}
+            {date.toLocaleString(undefined, {
+                weekday: "long",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+                day: "numeric",
+                month: "long"
+
+            })}
+        </div>
+    )
+}
+
+
+export default Clock;
