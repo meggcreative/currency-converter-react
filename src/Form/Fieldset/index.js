1c8f04cb9ac7bac1c0e6688cc4970265eba790ce
Magdalena Zulewska <madziapernach@gmail.com>
2021-06-28 17:09:46 +0200

    code formated

--- src/Form/Fieldset/index.js
+++ src/Form/Fieldset/index.js
@@ -1,4 +1,4 @@
-const Fieldset = ({legendContent, fieldsetBody}) => {
+const Fieldset = ({ legendContent, fieldsetBody }) => {
     return (
         <fieldset className="form__fieldset">
             <legend className="form__legend">{legendContent}</legend>
