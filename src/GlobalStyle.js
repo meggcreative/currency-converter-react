bf5611dae0aaf0c8821bd771dd0f9ecf156a2022
Magdalena Zulewska <madziapernach@gmail.com>
2021-07-06 20:29:06 +0200

    code format

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,27 +1,28 @@
 import { createGlobalStyle } from "styled-components";
 import background from "./images/backgroundImage.png";
+
 export const GlobalStyle = createGlobalStyle`
     html {
-  box-sizing: border-box;
-}
+        box-sizing: border-box;
+    }
 
-*,
-::after,
-::before {
-  box-sizing: inherit;
-}
+    *,
+    ::after,
+    ::before {
+        box-sizing: inherit;
+    }
 
-body {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: rgb(221, 240, 240);
-  padding: 20px;
-  margin: 0 auto;
-  font-family: 'Open Sans', sans-serif;
-  background-size: cover;
-  background-image: url("${background}");
-}
+    body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: rgb(221, 240, 240);
+        padding: 20px;
+        margin: 0 auto;
+        font-family: 'Open Sans', sans-serif;
+        background-size: cover;
+        background-image: url("${background}");
+    }
 
 `;
