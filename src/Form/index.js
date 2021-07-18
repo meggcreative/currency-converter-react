2c5b2a462aa0710e2c46eac9413aa2237b975f4c
Magdalena Zulewska <madziapernach@gmail.com>
2021-07-18 17:13:24 +0200

    added new view to action: loading, failure

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,65 +1,104 @@
 import React, { useState } from 'react';
 import Fieldset from "./Fieldset";
-import currencies from "../Currencies/currencies";
 import Result from "./Result";
-import { Select, Button } from "./styled.js"
+import { Select, Button, Loading, Failure, StyledImage } from "./styled.js";
+import {useRatesData} from "../useRatesData";
 
+const Form = () => {
 
-const Form = ({ calculateResult, result }) => {
+    const [result, setResult] = useState();
+    const ratesData = useRatesData();
+
+    const calculateResult = (amount, currency) => {
+        const rate = ratesData.rates[currency];
+
+        setResult({
+            givenAmount: +amount,
+            targetAmount: amount * rate,
+            currency,
+        });
+    }
 
     const [amount, setAmount] = useState();
-    const [currency, setCurrency] = useState(currencies[0].short);
+    const [currency, setCurrency] = useState("EUR");
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         calculateResult(amount, currency);
     }
 
+
     return (
         <form onSubmit={onFormSubmit}>
-            <Fieldset
-                legendContent="Na jaką walutę, którą chcesz zamienić PLN?"
-                fieldsetBody={
-                    <label>
-                        <Select
-                            value={currency}
-                            onChange={(event) => setCurrency(event.target.value)}
-                        >
-                            {currencies.map((currency => (
-                                <option
-                                    key={currency.short}
-                                    value={currency.short}
-                                >
-                                    {currency.label}
-                                </option>
-                            )))}
-                        </Select>
-                    </label>
-                }
-            />
-
-            <Fieldset
-                legendContent="Kwota którą chcesz przeliczyć ?"
-                fieldsetBody={
+            {ratesData.state === "loading"
+                ? (
                     <>
-                        Podaj kwotę w PLN:
-                        <Select as="input"
-                            value={amount}
-                            onChange={({ target }) => setAmount(target.value)}
-                            name="amount"
-                            type="number"
-                            step="0.01"
-                            required
-                        />
+                    <Fieldset
+                    legendContent="Upsss..."
+                    fieldsetBody={
+                        <>
+                        <Loading>
+                        Chwila moment... <br /> Ładowanie kursów walut z Europejskiego Banku Centralnego.
+                        </Loading> 
+                        <StyledImage />
+                </>
+                    }>
+                    </Fieldset>
                     </>
-                }
-            />
+                )
+                : (
+                    ratesData.state === "error" ? (
+                        <Failure>
+                            Wystąpił błąd! <br /> Sprawdź połączenie z internetem, jeżeli masz, błąd może być po naszej stronie. Spróbuj ponownie za jakiś czas.
+                        </Failure>
+                    ) : (
+                        <>
+                            <Fieldset
+                                legendContent="Na jaką walutę, którą chcesz zamienić PLN?"
+                                fieldsetBody={
+                                    <label>
+                                        <Select
+                                            value={currency}
+                                            onChange={(event) => setCurrency(event.target.value)}
+                                        >
+                                            {!!ratesData.rates && Object.keys(ratesData.rates).map(((currency) => (
+                                                <option
+                                                    key={currency}
+                                                    value={currency}
+                                                >
+                                                    {currency}
+                                                </option>
+                                            )))}
+                                        </Select>
+                                    </label>
+                                }
+                            />
 
-            <p>
-                <Button>Przelicz</Button>
-            </p>
+                            <Fieldset
+                                legendContent="Kwota którą chcesz przeliczyć ?"
+                                fieldsetBody={
+                                    <>
+                                        Podaj kwotę w PLN:
+                                        <Select as="input"
+                                            value={amount}
+                                            onChange={({ target }) => setAmount(target.value)}
+                                            name="amount"
+                                            type="number"
+                                            step="0.01"
+                                            required
+                                        />
+                                    </>
+                                }
+                            />
 
-            <Result result={result} />
+                            <p>
+                                <Button>Przelicz</Button>
+                            </p>
+
+                            <Result result={result} />
+                        </>
+                    )
+                )}
         </form>
     );
 };
